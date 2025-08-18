07f292821a1ba0d7ac08b19c0ed98266137fe0eb
Sedra Ramarosaona <sedranante@gmail.com>
2025-08-18 18:54:55 +0200

    Adding Parsing and Execution (simple : no args and no Path Yet)

--- shell_functions.c
+++ shell_functions.c
@@ -30,3 +30,63 @@ void free_input(t_input *input)
         input->line = NULL;
     }
 }
+
+
+void parse_command(t_input *input, t_parse *parse)
+{
+    if (input->line == NULL)
+    {
+        parse->command = NULL;
+        return;
+    }
+
+    /* Removing  \n  */
+    size_t len = strlen(input->line);
+    if (len > 0 && input->line[len - 1] == '\n')
+        input->line[len - 1] = '\0';
+
+    parse->command = strdup(input->line);
+    if (parse->command == NULL)
+    {
+        perror("Erreur allocation parse->command");
+    }
+}
+
+/* execution simple : fork + execve : (no arg no path) */
+void execute_command(t_parse *parse, t_execute *exec)
+{
+    pid_t pid;
+
+    if (parse->command == NULL || parse->command[0] == '\0')
+    {
+        exec->result = -1;
+        return;
+    }
+
+    pid = fork();
+    if (pid < 0)
+    {
+        perror("fork");
+        exec->result = -1;
+        return;
+    }
+
+    if (pid == 0)
+    {
+        /* Processus enfant */
+        char *args[] = {parse->command, NULL};
+        extern char **environ;
+        if (execve(parse->command, args, environ) == -1)
+        {
+            perror("execve");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        /* Processus parent */
+        int status;
+        waitpid(pid, &status, 0);
+        exec->result = (WIFEXITED(status)) ? WEXITSTATUS(status) : -1;
+    }
+}
