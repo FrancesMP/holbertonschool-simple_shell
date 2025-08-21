c2bb4864729664f89d659b8518eb4f346ba486c5
Sedra Ramarosaona <sedranante@gmail.com>
2025-08-21 21:10:00 +0200

    Fixing Betty

--- v4shell.c
+++ v4shell.c
@@ -5,10 +5,12 @@
 #include <string.h>
 #include <signal.h>
 
+
 void handle_sigint(int sig)
 {
-    (void)sig;
-    write(1, "\n$ ", 3); /* Réaffiche le prompt */
+    (void)sig;          
+    write(1, "\n$ ", 3); 
+    fflush(stdout);
 }
 
 int main(void)
@@ -21,26 +23,24 @@ int main(void)
     signal(SIGINT, handle_sigint);
 
     while (shell.running)
-    { 
-        if (isatty(STDIN_FILENO))
-        display_prompt(&shell.prompt);
-
-    if (read_input(&shell.input) == -1)
     {
+        
         if (isatty(STDIN_FILENO))
-            printf("\n");
-        break;
-    }
+            display_prompt(&shell.prompt);
+
+        if (read_input(&shell.input) == -1)
+        {
+            if (isatty(STDIN_FILENO)) 
+                printf("\n");
+            break;
+        }
 
         parse_command(&shell.input, &shell.parse);
 
         if (shell.parse.command != NULL)
         {
-            /* commandes internes */
             if (strcmp(shell.parse.command, "exit") == 0)
-            {
                 shell.running = 0;
-            }
             else if (strcmp(shell.parse.command, "cd") == 0)
             {
                 if (chdir("/") != 0)
