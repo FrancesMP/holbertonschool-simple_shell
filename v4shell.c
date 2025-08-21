88139333995e7010c992f9bbd95508ec63161e77
Sedra Ramarosaona <sedranante@gmail.com>
2025-08-21 21:32:49 +0200

    Fixing Betty

--- v4shell.c
+++ v4shell.c
@@ -5,11 +5,10 @@
 #include <string.h>
 #include <signal.h>
 
-
 void handle_sigint(int sig)
 {
-    (void)sig;          
-    write(1, "\n$ ", 3); 
+    (void)sig;
+    write(1, "\n$ ", 3);
     fflush(stdout);
 }
 
@@ -19,18 +18,23 @@ int main(void)
 
     shell.prompt.text = "$ ";
     shell.running = 1;
+    shell.input.line = NULL;
+    shell.input.len = 0;
+    shell.input.status = 0;
+    shell.parse.command = NULL;
+    shell.parse.argv = NULL;
 
     signal(SIGINT, handle_sigint);
 
     while (shell.running)
     {
-        
         if (isatty(STDIN_FILENO))
             display_prompt(&shell.prompt);
 
         if (read_input(&shell.input) == -1)
         {
-            if (isatty(STDIN_FILENO)) 
+            free_input(&shell.input);  /* ✅ évite fuite getline */
+            if (isatty(STDIN_FILENO))
                 printf("\n");
             break;
         }
@@ -40,11 +44,20 @@ int main(void)
         if (shell.parse.command != NULL)
         {
             if (strcmp(shell.parse.command, "exit") == 0)
+            {
                 shell.running = 0;
+            }
             else if (strcmp(shell.parse.command, "cd") == 0)
             {
-                if (chdir("/") != 0)
-                    perror("cd");
+                if (shell.parse.argv[1])  /* ✅ cd vers dossier demandé */
+                {
+                    if (chdir(shell.parse.argv[1]) != 0)
+                        perror("cd");
+                }
+                else
+                {
+                    chdir(getenv("HOME")); /* cd tout seul → HOME */
+                }
             }
             else
             {
@@ -52,9 +65,13 @@ int main(void)
             }
         }
 
-        free_input(&shell.input);
-        free_parse(&shell.parse);
+        free_input(&shell.input);  
+        free_parse(&shell.parse);  
     }
+
+    /* sécurité en sortie */
+    free_input(&shell.input);
+    free_parse(&shell.parse);
 
     return 0;
 }
