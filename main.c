12ea35ec334f31f17d85b6732f6e237920869311
Sedra Ramarosaona <sedranante@gmail.com>
2025-08-22 04:41:53 +0200

    betty fix

--- main.c
+++ main.c
@@ -5,18 +5,16 @@
 #include <unistd.h>
 #include <string.h>
 
-
 /**
  * handle_sigint - Handles Ctrl+C signal
  * @sig: Signal received
  *
  * Return: void
  */
-void handle_sigint(int sig)
-{
-    (void)sig;
-    write(1, "\n$ ", 3);
-    fflush(stdout);
+void handle_sigint(int sig) {
+  (void) sig;
+  write(1, "\n$ ", 3);
+  fflush(stdout);
 }
 
 /**
@@ -25,81 +23,68 @@ void handle_sigint(int sig)
  * Return: Shell exit status
  */
 
-int main(void)
-{
-    t_shell shell;
-    int exit_status = 0;
-    ssize_t nread;
+int main(void) {
+  t_shell shell;
+  int exit_status = 0;
+  ssize_t nread;
 
-    shell.prompt.text = "$ ";
-    shell.running = 1;
-    shell.input.line = NULL;
-    shell.input.len = 0;
-    shell.input.status = 0;
-    shell.parse.command = NULL;
-    shell.parse.argv = NULL;
-    shell.exec.result = 0;
+  shell.prompt.text = "$ ";
+  shell.running = 1;
+  shell.input.line = NULL;
+  shell.input.len = 0;
+  shell.input.status = 0;
+  shell.parse.command = NULL;
+  shell.parse.argv = NULL;
+  shell.exec.result = 0;
 
-    signal(SIGINT, handle_sigint);
+  signal(SIGINT, handle_sigint);
 
-    while (shell.running)
-    {
-        if (isatty(STDIN_FILENO))
-            display_prompt(&shell.prompt);
+  while (shell.running) {
+    if (isatty(STDIN_FILENO))
+      display_prompt( & shell.prompt);
 
-        nread = read_input(&shell.input);
-        if (nread == -1) /* EOF ou erreur */
-        {
-            free_input(&shell.input);
-            if (isatty(STDIN_FILENO))
-                printf("\n");
-            break;
-        }
-
-        if (shell.input.line[0] == '\0') /* Ligne vide */
-        {
-            free_input(&shell.input);
-            continue;
-        }
-
-        parse_command(&shell.input, &shell.parse);
-
-        if (shell.parse.command != NULL)
-        {
-            if (strcmp(shell.parse.command, "exit") == 0)
-            {
-                shell.running = 0;
-                exit_status = shell.exec.result;
-            }
-            else if (strcmp(shell.parse.command, "cd") == 0)
-            {
-                if (shell.parse.argv[1])
-                {
-                    if (chdir(shell.parse.argv[1]) != 0)
-                        perror("cd");
-                    shell.exec.result = (chdir(shell.parse.argv[1]) == 0) ? 0 : 1;
-                }
-                else
-                {
-                    chdir(getenv("HOME"));
-                    shell.exec.result = 0;
-                }
-            }
-            else
-            {
-                execute_command(&shell.parse, &shell.exec);
-            }
-        }
-
-        free_input(&shell.input);
-        free_parse(&shell.parse);
-
-        if (!isatty(STDIN_FILENO)) /* Mode non-interactif : sortir après une commande */
-            break;
+    nread = read_input( & shell.input);
+    if (nread == -1) /* EOF ou erreur */ {
+      free_input( & shell.input);
+      if (isatty(STDIN_FILENO))
+        printf("\n");
+      break;
     }
 
-    free_input(&shell.input);
-    free_parse(&shell.parse);
+    if (shell.input.line[0] == '\0') /* Ligne vide */ {
+      free_input( & shell.input);
+      continue;
+    }
 
-    return exit_status;
+    parse_command( & shell.input, & shell.parse);
+
+    if (shell.parse.command != NULL) {
+      if (strcmp(shell.parse.command, "exit") == 0) {
+        shell.running = 0;
+        exit_status = shell.exec.result;
+      } else if (strcmp(shell.parse.command, "cd") == 0) {
+        if (shell.parse.argv[1]) {
+          if (chdir(shell.parse.argv[1]) != 0)
+            perror("cd");
+          shell.exec.result = (chdir(shell.parse.argv[1]) == 0) ? 0 : 1;
+        } else {
+          chdir(getenv("HOME"));
+          shell.exec.result = 0;
+        }
+      } else {
+        execute_command( & shell.parse, & shell.exec);
+      }
+    }
+
+    free_input( & shell.input);
+    free_parse( & shell.parse);
+
+    if (!isatty(STDIN_FILENO)) /* Mode non-interactif : sortir après une commande */
+      break;
+  }
+
+  free_input( & shell.input);
+  free_parse( & shell.parse);
+
+  return exit_status;
 }
